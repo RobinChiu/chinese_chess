3387cf26697830c6eb2892529f2d63cf2433ef0f
Robin Chiu <hcchiu63@gmail.com>
2025-06-02 14:53:57 +0800

    remove piece will remove piece event also.

--- src/script.js
+++ src/script.js
@@ -26,6 +26,49 @@ function removePiece(piece) {
         deadblackCount++;
     }
     piece.setAttribute('transform', `translate(${x},${y})`);
+    // remove the piece event listener
+    piece.removeEventListener('click', handleClick);
+}
+
+function handleClick(e) {
+    const piece = e.currentTarget;
+    e.stopPropagation();
+    if (selected) {
+        // check the selected piece data-color
+        // same colore switch selected piece
+        if (selected.getAttribute('data-color') === piece.getAttribute('data-color')) {
+            selected.querySelectorAll('circle').forEach(circle => {
+                circle.setAttribute('fill', 'white');
+            });
+            selected = piece;
+            // different color remove piece and move selected piece
+        } else {
+            const transform = piece.getAttribute('transform');
+            const match = transform.match(/translate\(([^,]+),([^)]+)\)/);
+            let x, y = 0;
+            if (match) {
+                x = parseFloat(match[1]);
+                y = parseFloat(match[2]);
+            }
+            console.log(`x: ${x}, y: ${y}`);
+            // remove the piece from the board
+            removePiece(piece);
+            // move the selected piece to the clicked position
+            selected.querySelectorAll('circle').forEach(circle => {
+                circle.setAttribute('fill', 'white');
+            });
+            selected.setAttribute('transform', `translate(${x},${y})`);
+            selected = null
+        }
+    } else {
+        selected = piece;
+    }
+
+    if (selected) {
+        selected.querySelectorAll('circle').forEach(circle => {
+            circle.setAttribute('fill', 'yellow');
+        });
+    }
 }
 
 export function setupMouseMoveAndClick(document) {
@@ -34,45 +77,7 @@ export function setupMouseMoveAndClick(document) {
         const pieces = svgDoc.querySelectorAll('.piece');
 
         pieces.forEach(piece => {
-            piece.addEventListener('click', e => {
-                e.stopPropagation();
-                if (selected) {
-                    // check the selected piece data-color
-                    // same colore switch selected piece
-                    if (selected.getAttribute('data-color') === piece.getAttribute('data-color')) {
-                        selected.querySelectorAll('circle').forEach(circle => {
-                            circle.setAttribute('fill', 'white');
-                        });
-                        selected = piece;
-                        // different color remove piece and move selected piece
-                    } else {
-                        const transform = piece.getAttribute('transform');
-                        const match = transform.match(/translate\(([^,]+),([^)]+)\)/);
-                        let x, y = 0;
-                        if (match) {
-                            x = parseFloat(match[1]);
-                            y = parseFloat(match[2]);
-                        }
-                        console.log(`x: ${x}, y: ${y}`);
-                        // remove the piece from the board
-                        removePiece(piece);
-                        // move the selected piece to the clicked position
-                        selected.querySelectorAll('circle').forEach(circle => {
-                            circle.setAttribute('fill', 'white');
-                        });
-                        selected.setAttribute('transform', `translate(${x},${y})`);
-                        selected = null
-                    }
-                } else {
-                    selected = piece;
-                }
-
-                if (selected) {
-                    selected.querySelectorAll('circle').forEach(circle => {
-                        circle.setAttribute('fill', 'yellow');
-                    });
-                }
-            });
+            piece.addEventListener('click', handleClick);
         });
 
         svgDoc.addEventListener('click', e => {
